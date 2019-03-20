4c53c0e8ba534eb08fff487d8713004f4cf59afe
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-03-20 13:29:02 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,13 @@
+def check():
+    try: 
+        a=int(input())
+        if(a>0):
+            print("positive")
+        elif(a==0):
+            print("zero")
+        else:
+            print("Negative")
+    except ValueError:
+            print("invalid")
+check()
+          
