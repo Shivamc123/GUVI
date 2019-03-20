ceb369d7651320c98077498449b987340ea717f7
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-03-20 13:32:13 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -2,7 +2,7 @@ def check():
     try: 
         a=int(input())
         if(a>0):
-            print("positive")
+            print("Positive")
         elif(a==0):
             print("zero")
         else:
