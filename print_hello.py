9ec425aec4569f4479878e1be87b4d805d9b6d72
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-04-07 10:08:25 +0530

    Create print_hello.py

---
+++ print_hello.py
@@ -0,0 +1,12 @@
+
+    
+def check():
+    try: 
+        n=int(input())
+        a="Hello"
+        for i in range(0,n):
+            print(a)
+    except ValueError:
+            print("invalid")
+check()
+        
