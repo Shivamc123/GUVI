ffb8e59f34d5be06e161082df2d0433a9a7a54f2
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-04-07 09:37:45 +0530

    Update 3numb.py

--- 3numb.py
+++ 3numb.py
@@ -1,2 +1,13 @@
-x=list(map(int,input().split()))
-print(max(x))
+    
+def alpha(x,y,z): 
+    if(x>y)and(y>z):
+        print(x)
+    elif(y>x)and(y>z):
+        print(y)
+    else:
+        print(z)
+        
+x=int(input())
+y=int(input())
+z=int(input())
+alpha(x,y,z) 
