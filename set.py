dc7f67a1de8681f45044fa543951d3984abbc098
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-04-15 10:05:07 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,19 @@
+def common_member(a, b): 
+    a_set = set(a) 
+    b_set = set(b) 
+    if (a_set & b_set): 
+        print("yes") 
+    else: 
+        print("no")  
+           
+n,m =[int(x) for x in input().split()]
+a = []
+b = input().split()
+for i in range(0,n):
+	a.append(b[i])
+c=[]
+d=input().split()
+for i in range(0,m):
+	c.append(d[i])
+common_member(a,c) 
+   
