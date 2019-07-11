0d619847154d75dc6a6d61c5838dc236179b5ea0
Shivamc123 <48746969+Shivamc123@users.noreply.github.com>
2019-07-11 18:44:14 +0530

    Rename pro7.py to ppro7.py

---
+++ ppro7.py
@@ -0,0 +1,16 @@
+y,z=1,2
+t=int(input())
+while z<=t:
+    if 2**y==t:
+        y=0
+        break
+    else:
+        z=2**y
+        y=y+1
+if(y!=0):
+    y=y-2
+    a=t-2**y
+    print(a)
+else:
+    print(0)
+    #shivam
